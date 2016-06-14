ded105c03491a9ee408b25828a1ba7fc338c0e0f
nbarman <namitmohanbarman@Namits-MacBook-Air.local>
2016-06-14 01:47:14 -0400

    Updates
    Updates as of jun 14

--- EazyEdu/app/src/main/java/com/eazyedu/ContactUs.java
+++ EazyEdu/app/src/main/java/com/eazyedu/ContactUs.java
@@ -2,6 +2,7 @@ package com.eazyedu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Menu;
@@ -9,16 +10,77 @@ import android.view.MenuItem;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import 	android.app.Fragment;
+import android.app.Activity;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class ContactUs extends AppCompatActivity {
+public class ContactUs extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
+        /*View backgroundImage = findViewById(R.id.);
+        Drawable background = backgroundImage.getBackground();
+        background.setAlpha(80); */
         setContentView(R.layout.content_contact_us);
+        addListenerOnButton();
+
     }
+
+
+    public void addListenerOnButton() {
+
+
+
+        Button button = (Button) findViewById(R.id.submit_button);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                EditText name_text = (EditText) findViewById(R.id.name_Text);
+                EditText email_text = (EditText) findViewById(R.id.email_Text);
+                EditText question_text = (EditText) findViewById(R.id.message_Text);
+
+                String name = name_text.getText().toString();
+                String email = email_text.getText().toString();
+                String question = question_text.getText().toString();
+
+                /**
+                 * Validation check
+                 */
+                 if (name.trim().length()==0
+                         || email.trim().length()==0
+                         || question.trim().length()==0){
+                     /**
+                      *  Some fields are empty. Request to fill in all fields
+                      */
+                     Toast.makeText(ContactUs.this,"Please fill up all the fields!",Toast.LENGTH_LONG).show();
+                 } else {
+                     /**
+                      *  The fields are populated. Send the Email message
+                      */
+
+                     name_text.getText().clear();
+                     email_text.getText().clear();
+                     question_text.getText().clear();
+
+                     Toast.makeText(ContactUs.this, "Your message has been submitted", Toast.LENGTH_LONG).show();
+                /*Intent browserIntent =
+                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
+                startActivity(browserIntent); */
+                 }
+            }
+
+        });
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
